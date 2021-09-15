152fa9b6e83b08ff9784bb58af87b78030287f2b
Lucas Ellery <lucas.ellery@somapay.com.br>
2021-09-15 18:00:06 -0300

    salvando no local storage

--- scripts.js
+++ scripts.js
@@ -21,31 +21,44 @@ function toggleModal() {
 
 // Incomes and expenses
 
-const transactions = [
-  {
-    description: "Luz",
-    amount: -500,
-    date: "23/01/2021",
+// const transactions = [
+//   {
+//     description: "Luz",
+//     amount: -500,
+//     date: "23/01/2021",
+//   },
+//   {
+//     description: "Criação de website",
+//     amount: 5000,
+//     date: "23/01/2021",
+//   },
+//   {
+//     description: "Internet",
+//     amount: -8000.0,
+//     date: "23/01/2021",
+//   },
+//   {
+//     description: "App",
+//     amount: 20000,
+//     date: "23/01/2021",
+//   },
+// ];
+
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
   },
-  {
-    description: "Criação de website",
-    amount: 5000,
-    date: "23/01/2021",
+
+  set(transactions) {
+    localStorage.setItem(
+      "dev.finances:transactions",
+      JSON.stringify(transactions)
+    );
   },
-  {
-    description: "Internet",
-    amount: -8000.0,
-    date: "23/01/2021",
-  },
-  {
-    description: "App",
-    amount: 20000,
-    date: "23/01/2021",
-  },
-];
+};
 
 const Transaction = {
-  all : transactions,
+  all : Storage.get(),
 
   add(transaction) {
     Transaction.all.push(transaction);
@@ -91,12 +104,14 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement('tr');
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr)
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSClasses = transaction.amount > 0 ? "income" : "expense";
     const amount = Utils.formatCurrency(transaction.amount)
 
@@ -105,7 +120,7 @@ const DOM = {
       <td class="${CSSClasses}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-        <img src="./assets/minus.svg" alt="Remover transação" />
+        <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação" />
       </td>
     `;
 
@@ -214,17 +229,18 @@ const Form = {
     } catch (error) {
       alert(error.message);
     }
-    
   }
 }
 
 const App = {
   init() {
-    Transaction.all.forEach(transaction => {
-      DOM.addTransaction(transaction);
+    Transaction.all.forEach((transaction, index) => {
+      DOM.addTransaction(transaction, index);
     })
 
     DOM.updateBalance()
+
+    Storage.set(Transaction.all)
   },
   reload(){
     DOM.clearTransactions();
@@ -235,4 +251,3 @@ const App = {
 
 App.init()
 
-// 2h12min
