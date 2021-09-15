f402bc0b0d8b7ff071e4e9f6093c5d9a4d2261ca
Lucas Ellery <lucas.ellery@somapay.com.br>
2021-09-15 17:19:45 -0300

    creating submit function

--- scripts.js
+++ scripts.js
@@ -143,6 +143,78 @@ const Utils = {
     })
     
     return signal + value
+  },
+  formatAmount(value) {
+    value = Number(value.replace(/\,\./g, "")) * 100;
+    
+    return value;
+  },
+  formatDate(date) {
+    const splittedDate = date.split("-");
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  }
+}
+
+const Form = {
+  description : document.querySelector('input#description'),
+  amount : document.querySelector('input#amount'),
+  date : document.querySelector('input#date'),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value, 
+    }
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+
+    // trim: removes the blank spaces
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos");
+    }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date
+    }
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault(); 
+
+    try {
+      Form.validateFields();
+      const transaction = Form.formatValues();
+      Transaction.add(transaction);
+
+      Form.clearFields();
+      modal.close();
+
+    } catch (error) {
+      alert(error.message);
+    }
+    
   }
 }
 
@@ -162,3 +234,5 @@ const App = {
 
 
 App.init()
+
+// 2h12min
